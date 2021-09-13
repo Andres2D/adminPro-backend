948c761dc2a1f1a2b82c46358b9eda2b9b74e73b
Andres2D <andres2d1997.1206@gmail.com>
2021-09-13 18:59:59 -0500

    Adding search by collection

--- routes/searchs.js
+++ routes/searchs.js
@@ -1,14 +1,15 @@
 /*
-    Route: '/api/all
+    Route: '/api/all/
 */
 
 const { Router } = require('express');
 const { validateJWT } = require('../middlewares/validate-jwt');
 
-const { getAll } = require('../controllers/searchs');
+const { getAll, getDocumentsCollection } = require('../controllers/searchs');
 
 const router = Router();
 
-router.get( '/', [validateJWT], getAll);
+router.get( '/:search', validateJWT, getAll);
+router.get( '/collection/:table/:search', validateJWT, getDocumentsCollection);
 
 module.exports = router;
