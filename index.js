a89ce794aadcc743f1d202c277538e4ad8c4e5a4
Andres2D <andres2d1997.1206@gmail.com>
2021-09-30 17:44:08 -0500

    Adding default route resolver

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path');
 
 const express = require('express');
 const cors = require('cors');
@@ -27,6 +28,11 @@ app.use('/api/doctors', require('./routes/doctor'));
 app.use('/api/login', require('./routes/auth'));
 app.use('/api/all', require('./routes/searchs'));
 app.use('/api/upload', require('./routes/uploads'));
+
+// The latest
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public/index.html'));
+});
 
 app.listen( process.env.PORT, () => {
   console.log('Server runnig on port ' + process.env.PORT );
